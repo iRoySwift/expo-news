d4ade91fcf9c686c42fcdf3c3c6e2b56648df705
Roy <iRoySwift@iCloud.com>
2024-11-10 23:21:53 +0800

    init nativewind theme

---
+++ hooks/useTailwind.ts
@@ -0,0 +1,10 @@
+import resolveConfig from "tailwindcss/resolveConfig";
+import { useMemo } from "react";
+const tailwindConfig = require("tailwind.config");
+
+const useTailwind = () => {
+    const tailwind = useMemo(() => resolveConfig(tailwindConfig), []);
+    return tailwind;
+};
+
+export default useTailwind;
