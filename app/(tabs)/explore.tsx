ad9c3e6c4f5788a4657365d62c86a905adbfcd63
Roy <iRoySwift@iCloud.com>
2024-12-08 14:13:35 +0800

    tabs

---
+++ app/(tabs)/explore.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+interface Props {}
+const ExploreScreen: React.FC<Props> = () => {
+  return (
+    <View>
+      <Text>ExploreScreen</Text>
+    </View>
+  );
+};
+export default ExploreScreen;
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,35 @@
+import { Link } from "expo-router";
+import React from "react";
+import { View, ScrollView } from "react-native";
+
+interface Props {}
+const HomeScreen: React.FC<Props> = () => {
+  return (
+    <View className="h-full w-full justify-center bg-white p-2">
+      <ScrollView>
+        <Link className="w-100 p-2" href="/learn/02-flexbox/flexDirection">
+          FlexDirection
+        </Link>
+        <Link className="w-100 p-2" href="/learn/02-flexbox/justifyContent">
+          JustifyContent
+        </Link>
+        <Link className="w-100 p-2" href="/learn/02-flexbox/alignItems">
+          AlignItem
+        </Link>
+        <Link className="w-100 p-2" href="/learn/02-flexbox/flex">
+          Flex
+        </Link>
+        <Link className="w-100 p-2" href="/learn/03-dimensions">
+          Demensions
+        </Link>
+        <Link className="w-100 p-2" href="/learn/04-alertbutton">
+          AlertButton
+        </Link>
+        <Link className="w-100 p-2" href="/learn/05-switch_statusbar">
+          Switch Status
+        </Link>
+      </ScrollView>
+    </View>
+  );
+};
+export default HomeScreen;
